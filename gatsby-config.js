9d89d6f43698b859b691f1409d3c6d4190b3a70b
dgiordano33 <dgiordano@wesleyan.edu>
2020-05-18 09:53:07 -0400

    May18 V1.13 Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         name: `markdown-pages`,
         path: `${__dirname}/_data`,
       },
-    },
+    }, 
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -41,6 +41,40 @@ module.exports = {
         head: true,
       }
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        // datalayer to be set before GTM is loaded
+        // should be a stringified object or object
+        // Defaults to null
+        defaultDataLayer: function () {
+          return {
+            pageType: window.pageType,
+          }
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WGVHHBJ",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -16,22 +16,7 @@ export default ({ children }) => {
     `
   )
   return (
-    <Helmet>
-    
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WGVHHBJ');</script>
-
-<link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=Jost:ital,wght@0,300;0,400;0,500;0,700;0,800;1,400&family=Merriweather:wght@300;400;700&family=Permanent+Marker&display=swap" rel="stylesheet">
-
-    </Helmet>
-    
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGVHHBJ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  
-
+   
     <div className="site-wrapper">
       <header className="site-header">
         <div className="site-title">
